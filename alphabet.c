fb03e7948bda5477cf03cb52f60f0bc4d82e6748
Manjuchikku <36722811+Manjuchikku@users.noreply.github.com>
2018-02-23 11:56:14 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("Enter the alphabet");
+scanf(%c,&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z"))
+{
+printf("alphabet");
+}
+else
+{
+printf("wrong");
+}
+return 0;
+}
