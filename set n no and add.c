affb5fa7aa0288f6dc61ee1ffbb344375eeeeaff
Manjuchikku <36722811+Manjuchikku@users.noreply.github.com>
2018-03-14 16:15:25 +0530

    Create set n no and add.c

---
+++ set n no and add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,N,k,s=0;
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)      
+printf("%d",i);
+scanf("%d",&k);
+for(j=1;j<=k;j++)
+
+s=s+j;
+printf("%d",s);
+    return 0;
+}
