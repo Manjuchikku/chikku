485d4fa53ee39fb5e7515084c98d5fb7119a1963
Manjuchikku <36722811+Manjuchikku@users.noreply.github.com>
2018-07-24 12:04:33 +0530

    Create multiples.c

---
+++ multiples.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+long int sum=0,n,i;
+printf("\n Enter the given value N:");
+scanf("%1d",%&n);
+for(i=1;i<n;i++)
+{
+printf("\n 5*%1d=%1d"i,5*i
+}
+return 0;
+}
