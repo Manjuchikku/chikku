06c8cb5accddae64e65887721adc1d656294f129
Manjuchikku <36722811+Manjuchikku@users.noreply.github.com>
2018-08-29 15:46:56 +0530

    Create mun.c

---
+++ mun.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int num;
+printf("\nEnter a number ");
+scanf("%d",&num);
+if(num*13==0)
+{
+printf("\nThe entered no is multiple by 13");
+}
+else
+{
+printf("\nThe entered no is not multiple by 13");
+}
+getch();
+}
